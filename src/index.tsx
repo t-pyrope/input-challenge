150f93a4fba687d8c3da6ef4cde07c0470773d0d
t-pyrope <kimmarina155@gmail.com>
2021-10-21 13:28:56 +0200

    Add devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
 import rootReducer from './reducers';
 import './assets/styles/main.scss';
 
-const store = createStore(rootReducer);
+declare global {
+    interface Window {
+      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as typeof compose || compose;
+
+const store = createStore(rootReducer, composeEnhancers());
 
 // "Whatever is being returned from store.getState, I want the types stored ad the RootState variable" 
 // https://stackoverflow.com/a/67656911/13891817
